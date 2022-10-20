d1a24573b975842ea46ac60a5826cba9e57376a6
EmiratD <lamzar031408@gmail.com>
2022-10-20 14:55:52 +0600

    randon num

--- src/components/bord/Bord.jsx
+++ src/components/bord/Bord.jsx
@@ -1,35 +1,70 @@
-import React from 'react';
-import './bord.scss';
-import Cill from '../cill/Cill';
-import { useState } from 'react';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import cloneDeep from "lodash.clonedeep";
 
+import Cill from "../cill/Cill";
+
+import "./bord.scss";
 
 const Bord = (props) => {
-
   let mas = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [0, 0, 2, 0],
     [0, 0, 0, 0],
   ];
 
   const [arr, setArr] = useState(mas);
 
+  // рендер плиток
   const arrTile = [];
-
-  arr.map((el1, idx1)=> el1.map((el2,idx2) => {
-    if()
-    arrTile.push(<Cill className={'tile ' + 'tile'+el2} id={idx1+'-'+idx2} vel={el2} key={idx1+'-'+idx2}/>);
-  }));
-
-
-
-
-  return (
-    <div className='bord'>
-      {arrTile}
-    </div>
+  arr.map((el1, idx1) =>
+    el1.map((el2, idx2) => {
+      const value = el2 === 0 ? null : el2;
+      arrTile.push(
+        <Cill
+          cName={`tile tile${el2}`}
+          id={`${idx1}-${idx2}`}
+          vel={value}
+          key={`${idx1}-${idx2}`}
+        />
+      );
+    })
   );
-}
+
+  // рандоные появление цифр
+  const getRandomNum = () => {
+    let newMatrix = cloneDeep(arr);
+    createRandomNum(newMatrix);
+    createRandomNum(newMatrix);
+    setArr(newMatrix);
+    console.log(newMatrix);
+  };
+  // рандоиные месио и число 2 или 4
+  const createRandomNum = (newMatrix) => {
+    let added = false;
+    let gridFull = false;
+    let go = 0;
+    while (!added) {
+      if (gridFull) {
+        break;
+      }
+
+      let rand1 = Math.floor(Math.random() * 4);
+      let rand2 = Math.floor(Math.random() * 4);
+      go++;
+      if (newMatrix[rand1][rand2] === 0) {
+        newMatrix[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
+        added = true;
+      }
+    }
+  };
+
+  useEffect(() => {getRandomNum()}, []);
+
+  return <div className="bord" onClick={()=>{
+    
+  }}>{arrTile}</div>;
+};
 
 export default Bord;
